e352f3948e7daf23aeb0fd2ad2d4b3c3b65d6874
Adam Fenwick <afenwickdev@gmail.com>
2018-01-09 20:32:36 +0000

    Creation of first table

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Responsive & Accessible tables',
 
   // Default port that prototype runs on
   port: '3000',
